33bc7562d98b1b9ae5bda2ae83738d5135f1d4f4
David Ragsdale <me@davidjragsdale.com>
2019-10-26 21:22:17 -0400

    Add sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,15 +4,17 @@ module.exports = {
       resolve: `gatsby-theme-blog`,
       options: {},
     },
+    `gatsby-plugin-sitemap`,
   ],
   // Customize your site metadata:
   siteMetadata: {
+    siteUrl: `https://www.example.com`,
     title: `David Ragsdale dev`,
     author: `David Ragsdale`,
     description: `Dependency Injection and Inversion of Control in client JavaScript`,
     social: [
       {
-        name: `github`,
+        name: `Github`,
         url: `https://github.com/djragsdale/di-examples`,
       },
     ],
