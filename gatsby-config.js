20b88450655df6a6e96c9a05ebf7e88f3bd89148
David Ragsdale <me@davidjragsdale.com>
2019-10-26 20:22:03 -0400

    Add gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {},
+    },
+  ],
+  // Customize your site metadata:
+  siteMetadata: {
+    title: `Testable Vanilla JavaScript`,
+    author: `David Ragsdale`,
+    description: `Dependency Injection and Inversion of Control in client JavaScript`,
+    social: [
+      {
+        name: `github`,
+        url: `https://github.com/djragsdale/di-examples`,
+      },
+    ],
+  },
+}
