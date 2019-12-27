10f9e39d529b78e3a76134e0ddf5c5c2ec5a14ca
David Ragsdale <me@davidjragsdale.com>
2019-12-27 01:07:51 -0500

    Add runnable angularjs example

--- src/components/runnable-example.js
+++ src/components/runnable-example.js
@@ -126,6 +126,34 @@ function createInjector(dependencies) {
     });
   }
 }
+
+function angularJsInjector(dependencies, fn) {
+  var fnDependencies = fn.$inject || [];
+  var $inject = fnDependencies.slice(0);
+
+  while (fnDependencies.length > 0) {
+    var dependencyName = fnDependencies.shift();
+    if (!dependencies[dependencyName]) {
+      throw new Error('Dependency "' + dependencyName + '" is not a valid dependency.');
+    }
+    fn = fn.bind(null, dependencies[dependencyName]);
+  }
+
+  fn.$inject = $inject;
+  return fn;
+}
+
+function injectAngularJsContext(dependencies) {
+  var context = {
+    angularJsFunction: angularJsFunction,
+  };
+
+  Object.keys(context).forEach(function (key) {
+    context[key] = angularJsInjector(dependencies, context[key]);
+  });
+
+  return context;
+}
 `;
 
 export default ({
